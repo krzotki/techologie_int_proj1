1dd7293a6fdc32925c2152ad041213d3bf496d2d
karol <krzotki@gmail.com>
2021-04-01 20:14:36 +0200

    layout changes, post styles

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,36 +3,61 @@ import Aux from '../../hoc/ReactAux';
 import {NavLink} from 'react-router-dom';
 import './Layout.css';
 
-const layout = (props) => (
-    <Aux>
-        <header>
-            <div className = "logo container">
+class Layout extends React.Component {
+    
+    state = {
+        leftPanelVisible: true,
+    }
 
-            </div>
-            <nav>
-                <ul>
-                    <li> <NavLink to='/' exact>Home</NavLink> </li>
-                    <li> <NavLink to='/friends' exact>Friends</NavLink> </li>
-                    <li> <NavLink to='/temp' exact>Temp</NavLink> </li>
-                    <li> <NavLink to='/profile' exact>Profile</NavLink> </li>
-                </ul>
-            </nav>
-        </header>
+    showHideLeftPanel() {
+        this.setState({
+            leftPanelVisible: (!this.state.leftPanelVisible)
+        });
+    }
 
-        <main className = 'main'>
-            <section className = 'panel left'>
-                Left navigation
-            </section>
+    render() {
+        const leftPanelCLasses = ['panel', 'left'];
+        if(this.state.leftPanelVisible) {
+            leftPanelCLasses.push('visible');
+        }
 
-            <section className = 'page_content'>
-                {props.children}
-            </section>
-            
-            <section className = 'panel right'>
-                Friends list
-            </section>
-        </main>
-    </Aux>
-)
+        return(
+        <Aux>
+            <header>
+                <button className='menu' onClick={() => this.showHideLeftPanel()}>
+                    <i className="fa fa-bars"></i>
+                </button>
+    
+                <div className = "logo container">
+                    <NavLink to='/' exact>
+                        <img src='/images/logo.png'/>
+                    </NavLink>
+                </div>
+        
+                <nav>
+                    <ul>
+                        <li> <NavLink to='/friends' exact>Friends</NavLink> </li>
+                        <li> <NavLink to='/temp' exact>Temp</NavLink> </li>
+                        <li> <NavLink to='/profile' exact>Profile</NavLink> </li>
+                    </ul>
+                </nav>
+            </header>
+    
+            <main className = 'main'>
+                <section className = {leftPanelCLasses.join(' ')}>
+                    Left navigation
+                </section>
+    
+                <section className = 'page_content'>
+                    {this.props.children}
+                </section>
+                
+                <section className = 'panel right'>
+                    Friends list
+                </section>
+            </main>
+        </Aux>);
+    }
+}
 
-export default layout;
+export default Layout;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './Post.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 const Post = (props) => {
     console.log(props)
     return (
-        <div className='post_item' onClick={() => props.clicked()}>
+        <div className='post_item'>
             <div className='header'>
                 <h3>{props.title}</h3>
             </div>
 
             <div className='body'>
                 {props.children}
+            </div>
+
+            <hr/>
+
+            <div className='footer'>
+                <button className='like'><i className="fa fa-thumbs-up"></i></button>
+                <button className='comments' onClick={() => props.clicked()}><i className="fa fa-comment"></i></button>
+                <button className='share'><i className="fa fa-share-alt"></i></button>
+
             </div>
         </div>
     );
